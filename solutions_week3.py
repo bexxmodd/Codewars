d652437ba8b7ffc00ee540c65cf23d3ba62f025a
Bexx Modd <bexxmodd@seas.upenn.edu>
2020-05-16 14:46:08 -0400

    small text formatting changes

--- solutions_week3.py
+++ solutions_week3.py
@@ -4,7 +4,7 @@ def find_outlier(integers):
     odds = [n for n in integers if n % 2 != 0]
     return odds[0] if len(odds)<len(evens) else evens[0]
 
-print('\n** Find the Parity Outlier => 6 kyu **')
+print('\n** Find the Parity Outlier => 6 kyu! **')
 print(find_outlier([2, 4, 0, 100, 4, 11, 2602, 36]))
 print(find_outlier([160, 3, 1719, 19, 11, 13, -21]))
 print(find_outlier([2017237, 9223811, 7987298, 7298917, 5446159, 7787747, -1159841, -3690463, 7787767, 5141195, -259751, -7235503]))
@@ -20,7 +20,7 @@ def bouncingBall(h, bounce, window):
         return count + 1
     else:
         return -1
-print('\n** Bouncing Balls => 6 kyu **')
+print('\n** Bouncing Balls => 6 kyu! **')
 print(bouncingBall(3, 0.66, 1.5))
 print(bouncingBall(30, 0.66, 1.5))
 print(bouncingBall(3, 1, 1.5))
